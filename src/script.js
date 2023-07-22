b78574578dc7f23d451c1d1730c256c933c861c1
zainabw17 <127432552+zainabw17@users.noreply.github.com>
2023-07-22 18:38:10 +0500

    nav bar

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
 const button = document.getElementById("btn");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   alert("Button clicked!");
 });
 
 const paragraph = document.getElementById("demo");
 
-paragraph.addEventListener("mouseover", function() {
+paragraph.addEventListener("mouseover", function () {
   paragraph.style.color = "blue";
 });
 
-paragraph.addEventListener("mouseout", function() {
+paragraph.addEventListener("mouseout", function () {
   paragraph.style.color = "black";
 });
